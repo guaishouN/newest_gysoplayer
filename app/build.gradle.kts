abd8ec87c514afb2b6f550b3683d77062ebff16a
GuihuiDeng <Guihui.Deng@desaysv.com>
2024-12-23 10:19:16 +0800

    gradle rm implementation full ffmepeg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,5 +61,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.arthenica:mobile-ffmpeg-full:4.4.LTS")
 }
